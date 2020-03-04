b547a5b4bf0a91f5c9be40c72cb6170bf0b10688
Daniel Kim <daniel.kim52@gmail.com>
2020-03-04 12:27:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,14 +12,3 @@ def key_for_min_value(name_hash)
   end
   lowest_name
 end
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
-  lowest_key
-end
